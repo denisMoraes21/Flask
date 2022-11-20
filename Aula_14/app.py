f66bf89b044089c5168c3b8cd8b4e55571a08a6a
Denis Moraes <denisguimaraes2012@gmail.com>
2022-11-19 21:29:05 -0400

    Aula 14 completa

--- Aula_14/app.py
+++ Aula_14/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="template")
@@ -19,6 +19,34 @@ class Estudante(db.Model):
 def index():
         estudantes = Estudante.query.all()
         return render_template("index.html", estudantes=estudantes)
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+        if request.method == "POST":
+                estudante = Estudante(request.form["nome"], request.form["idade"])
+                db.session.add(estudante)
+                db.session.commit()
+                return redirect(url_for("index"))
+        return render_template("add.html")
+
+@app.route("/delete/<int:id>")
+def delete(id):
+        estudante = Estudante.query.get(id)
+        db.session.delete(estudante)
+        db.session.commit()
+        return redirect(url_for("index"))
+
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+        estudante = Estudante.query.get(id)
+        if request.method == "POST":
+                estudante.nome = request.form["nome"]
+                estudante.idade = request.form["idade"]
+                db.session.commit()
+                return redirect(url_for("index"))
+        return render_template("edit.html", estudante=estudante)
+
+
 
 if __name__ == 'main':
         db.create_all()
